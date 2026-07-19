99585453398c710f6323ac4632577a6b5baf6ac8
Balu <balu.www.55@gmail.com>
2026-07-20 00:45:22 +0530

    Add files via upload

---
+++ intro.c
@@ -0,0 +1,54 @@
+// Introduction to data structures and algorithms in c programming language 
+// 1. what is data structure ?
+// data structure is mathematical and logical way of organizing / storing data .
+
+// mathematical ?
+// predefined and organizing of data is known as mathematical way.
+
+// logical ?
+// logical is the way thinking and doing the operations is knows as logical way .
+
+// there are two types of data structures :
+// 1. linear data structures :
+// 2. non linear data structures :
+
+// linear data structures:
+// linear data structures where the data is linearly / sequentially arranged .
+// eg: array , linkedlist, stack , queue .
+
+// non linear data structures :
+// non linear data structure is where the data is non linearly/ non sequentially arranged. 
+// eg: tree , graph, heap, hashtable.
+
+// classification of data structures are mainly three types :
+// 1. container data structure = which stores the data .
+// eg : array linkedlist
+
+// 2. indexing data structure = finds the specific position of data 
+// eg: binary search tree , avl tree, etc
+
+// 3. priority data structure = which gives the data priority.
+// eg: queue . stack 
+
+// And here is the syllabus for gate:
+// 1. arrays :
+// 2. linkedlist :
+// 3. stack : 1) expression 2) recursion
+// 4. queue :
+// 5. tree :
+// 6. graph :
+// 7 hashing :
+
+// these we have to cover for the gate syllabus :
+
+// And there are the some operetions in data structures:
+// 1. Traversing = touching the every element atleast or atmost once .
+// 2. Insertion = inserting element.(overflow: we cannot add more element more than its size )
+// 3. deletion = removing/deleting an element.(underflow: we cannot delete the element less than size )
+// 4. searching = searching of an element. (sucessfull : we can search the element if possible . unsuccessfull : we cannot search the element if its not possible .)
+// 5. merging = combines same type of data .
+// 6. sorting = sorting and data into ascending or descending order .
+
+// these are introduction of data structures and algorithms in c programming language of dsa .
+
+
