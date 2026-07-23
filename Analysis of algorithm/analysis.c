1de26ea4d787da3c78de6af439a5fe9a898a0f05
Balu <balu.www.55@gmail.com>
2026-07-23 15:52:22 +0530

    Add files via upload

---
+++ Analysis of algorithm/analysis.c
@@ -0,0 +1,26 @@
+// Analysis of algorithm :
+// what is the analysis of algorithm 
+// algorithm is known as step by step process to to solve the problems 
+// analysis of algorithm is known as best method to solve the problem through code .
+// when there are mutliple problems we used analysis of algorithm to solve the problems and it is also known as order of growth.
+
+// base :
+// 1) space complexity 
+// 2) run time complexity 
+
+// 1) what is space time complexity ?
+// space time complexity is the no of elements requireed to input on the memory 
+// eg: no of memory requiured to space it is known as space time complexity 
+
+// 2) what is run time complexity ?
+// run time complexity it is no elements required to get output at particular time .
+// eg: time complexity is the time required to run code and get code 
+
+// we have calculations on time complexity eg: wall clock time , and number of time operations 
+// wall clock time == time required to run an algorithm. 
+// no of operations == steps required to run an algorithm at a particular time / may not be .
+
+
+// rate of growth/ order of growth .
+// the calculation of growth of an algorithm at what time complexity increases.
+// with what time the run time complexity increases as the increment of input in size it is known as order of growth 
