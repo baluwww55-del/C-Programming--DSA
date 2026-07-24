13e2cccf78089cdf22ab0c8ed5ff550aa759b02c
Balu <balu.www.55@gmail.com>
2026-07-25 00:51:02 +0530

    Add files via upload

---
+++ Analysis of algorithm/asymptotic notaions.c
@@ -0,0 +1,28 @@
+// Asymptotic notations :
+// asymptotic notations is one the important concept which is to found the bound of an algorithm .
+// we have classification of asymptotic notations :
+// 1) Big o notations :
+// 2) omegha notations :
+// 3) theta notations :
+
+// 1) Big O notations : is known as the tighest upper bound , it is the highest / maximum time or input taken by an algorithm .
+// lets understand with an example : if an person can be hunger for 7 days not more than tht if he he would die tht 7 is the maximum it is known as tighest bound .
+
+// 2) omega notations : is known as the tightest lower bound , it is the lowest / minimum time taken by an algorithm .
+// eg: a person can travel 100 km with in one hour not less than tht it can be more but it cant be lesser so it is knows as minimum or lower bound / omoega notations .
+
+// theta notations : it is known as average bound 
+// eg : if i distribute 100 choclate to 100 students it cant be 101 or 99 it cant be lower or it cant be upper 
+// it is also known as the exact bound of an algorithm . 
+
+/**
+we have two cases 
+1) best cases 
+2) worst case 
+
+1) best case is known as algorithm which takes min time to get output incase of larger output .
+
+2) worst case is known as algortihm which takes max time to get output wheter it is small input or large input . 
+/*
+
+// these are all the examples of asymptotic notations in next lecture we will define algotihm how we have to identify time complexity of an algorithm . 
