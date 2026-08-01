76e8da04a1d2c645e449f10709b38ec4b5c36c58
Balu <balu.www.55@gmail.com>
2026-08-02 02:02:04 +0530

    Add files via upload

---
+++ Arrays/2.Traversal in array.c
@@ -0,0 +1,26 @@
+// Traversal in an array is a  fundamental operation which allows to check each element atleast or atmost once .
+
+
+// Traversing in data structures using a c programming language :
+// Traversing : visiting every element / node  atleast or atmost once in a data structure . 
+// in an array we can traverse like this using c programming language :
+
+// this is declaration for travesing an arry .
+/**
+for(i=lb; i<=ub;i++)
+{
+visit[i];
+}
+runtime complexity will be O(n) where n is the number of elements in an array.
+space complexity will be O(1) becasue we are not using any extra space for traversing an array.
+there is no extra space ryt because we are using array . array = fixed size in data structure so the space complexity will be O(1).
+
+/**/
+
+// ub == upper bound of an array 
+// lb == lower bound of an array 
+// here lb starting ub o last from first to last it is doing ++++
+
+// so this is the example and best way to explain the traversing in an array using c programming language :
+
+// traversal is an important data structure operation because it allows to visit each element and easy operations and error detection in data structures will be easily done . 
