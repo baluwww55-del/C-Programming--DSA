941616c250831cb1ac7b99af9b11a3a060a1c980
Balu <balu.www.55@gmail.com>
2026-08-04 12:56:02 +0530

    Add files via upload

---
+++ Arrays/4. finding max in array.c
@@ -0,0 +1,33 @@
+// Finding the maximum element in an given array:
+// finding the maximum value in an given array:
+// assume tht we have an array [ 5,8,9,12,14,6,8,9]
+// for this we have to compare each element with the next element we have to do traversing for tht :
+
+// we have algorithm for traversing 
+/**
+{
+    visit[i];
+}
+/*/
+
+// for to find the maximum value of an array we have another algorithms we will perform tht 
+// eg array = [18,9,2,3,4,5,1]
+
+
+/*
+max = a[lb];              // lb as the first element of an array 
+for(i=lb+1;i<=ub;i++)     // for to compare the loop we are taking lb+1 to ub for to check the each element and to find the maximum value.
+{
+    if(a[i]>max)          // giving a condition if the a[i] = lb which is first element if greater amongn the finded value print tht.
+    {
+        max = a[i];
+    }
+    return max;          // no means return the element into an array which is greater in an array.
+}
+/**
+
+ // time  complexity will be theta (n) because it is taking linearly ryt i++ from lb to ub thats y linear time complexity.
+ // space complexity will be theta(1) because we are  not using any extra space for to find the maximum of an array. 
+
+
+// this is the simple example to find the minimum and maximum of an algorithms using c programming language :
