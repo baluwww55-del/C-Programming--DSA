76c34d710964aaec1976a27ac2828c3120ed81a5
Balu <balu.www.55@gmail.com>
2026-08-07 04:18:33 +0530

    Add files via upload

---
+++ Arrays/8.linear search in an array.c
@@ -0,0 +1,17 @@
+// Linear search in an array :
+// searching an element in an array . 
+// for tht we need to declare the input / find the inputs what we need 
+
+// for tht first we need an array lb ub and item as well 
+//  the logic is we will compare the item with index 
+// algorithm :
+{
+    for(i=lb;i<=ub;i++)
+    {
+        if(a[i] == item)
+        return i 
+    }
+}
+return lb-1
+// so this is the linear search in an array used to find the element in an array 
+// the run time complexity of this algorithms is O(n) is the run time complexity of this algorithm. 
