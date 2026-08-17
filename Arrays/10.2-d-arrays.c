3b2c3c68548e716663c9212547bf797c2db722c2
Balu <balu.www.55@gmail.com>
2026-08-17 22:22:31 +0530

    Add files via upload

---
+++ Arrays/10.2-d-arrays.c
@@ -0,0 +1,33 @@
+// 2-D arrays :
+// 2d arrays is a linear data structure :
+// 2d arrays is a collection of 1d arrays and it is used to store the matrix rows and columns 
+
+// declaration of 2d array :
+// datatype name [size] [size];
+// this is the declaration of 2d arrays .
+
+// eg :
+// int a[3][4];
+// this is the real example of two d arrays 
+// this is the declaration of 2d arrays in c programming language :
+
+// 2-d array in data structure :
+// datatype name [lb:ub][lb:ub] first one = row , second one = column 
+
+// formula to find the no of rows == ub - lb + 1
+// formula to find the no of columns == ub - lb + 1
+
+// these are the two columns of an array 
+
+// 2-d array storage :
+// 1) row major order :
+// 2) column major order :
+
+// to find the location and adress of an element :
+// for row  major order == loc([i][j]) == base + w*(( i-lbi) * n + (j - lbj))
+// for column major order == loc([i][j]) == base + w*((j-lbi) * n + (i - lbi))
+
+// this is the represenetation and formula of an 2-d arrays 
+// row major order 
+// column major order 
+// these are the two major storage of 2d arrays .
