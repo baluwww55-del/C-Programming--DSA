ce039608a1084da8e555d987508d7925f54a662c
Balu <balu.www.55@gmail.com>
2026-08-19 08:35:14 +0530

    Add files via upload

---
+++ Arrays/11. traversing code in c .c
@@ -0,0 +1,36 @@
+// Traversing code in c 
+// eg:
+/*#include<stdio.h>
+int main()
+{
+    int a[5] = {1,2,3,4,5};
+    for(int i = 0; i<5; i++)
+    {
+        printf("%d", a[i]);
+    }
+    return 0;
+}
+// basic code in arrays :
+/**/ 
+#include<stdio.h>
+void main()
+{
+    int arr[100];
+    int n;
+
+    printf("enter the elements :");
+    scanf("%d", &n);
+    printf("enter %d elements \n", n);
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("array elements are :\n");
+    for( int i=0; i<n; i++)
+    {
+        printf("%d", arr[i]);
+    }
+    
+}
