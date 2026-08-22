1fce758474a92831a548f5c81251d7a8b3c858de
Balu <balu.www.55@gmail.com>
2026-08-22 22:17:05 +0530

    Add files via upload

---
+++ Linked list/1.intro.c
@@ -0,0 +1,42 @@
+// Introduction to the linked list :
+// linked list is linear data structure 
+// which is memory allocated dynamically 
+// linked list can be accesed by using pointers :   
+
+
+// linked list contains nodes :
+// nodes contains two fields :
+// 1) elements(data)
+// 2) link(pointer)    pointer = which stores adress of another variable . 
+
+// there are some condition in linked list :
+// if(start == NUll)  or if(!start)
+// {
+//      empty list ;
+// }
+
+
+// if(start! == null) or if(start)
+// {
+//        non empty list;
+// }
+
+// if (!start --> link) or (start --> link == null)
+// {
+//       single node;
+// }
+
+// pointer dereferencing 
+// if we are trying to access the null node through a link  then it is known as pointer dereference and error comes as runtime error . 
+
+// link assignment 
+// link assignment is concept which is used to accces a link . 
+//Link assignment :-
+
+//    Start -> [ 9 |  ] -> [ 10 |  ] -> [ 11 |  ]
+//              ^ P
+//                |
+//     Start -> link = p;  = 9_
+
+// this is the concept of link 
+// and here completes the introduction of a linked list .
