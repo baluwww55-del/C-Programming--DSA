ee1f7d8d1e4540e74d7902668c21d1a144ff1b96
Balu <balu.www.55@gmail.com>
2026-09-06 12:23:10 +0530

    Update 002.enqueue.c

--- Queue/002.enqueue.c
+++ Queue/002.enqueue.c
@@ -1,1 +1,39 @@
 
+// Implementation using an array in an queue data structure :
+
+// in an queue we use implementation using an array to store the elements of the queue. 
+// we use to indexes which is used for operation : insertion and deletion opearations .
+// 1) front 
+// 2) rear 
+
+// the first element of the queue is known as the front 
+// the last element of the queue is known as the rear 
+
+// there are certain concepts when we use an array to implement data structure :
+
+
+// 1) when the array is an empty :
+
+front = -1
+rear = -1
+
+// when an single element in an array :
+
+front = 0
+rear = 0
+
+// when an array is full :
+
+front = 0 
+rear = size/n-1
+
+// so basically the concept of overflow comes when the queue is full 
+
+// so what is  overflow and what is underflow :
+// 1) overflow :
+         when an array is full and we have to insert an element and there is no space in an queue then the overflow concept occurs . 
+
+
+// 2) so what is underflow :
+// 2) underflow :
+            when an array empty and we have to delete the element and there is no element in an queue then occurs the concept of underflow .
