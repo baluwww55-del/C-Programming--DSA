d7e3fbeb70d5da1c20f97acea8f11ed34f27acf1
Balu <balu.www.55@gmail.com>
2026-09-06 11:10:57 +0530

    Add files via upload

---
+++ Queue/3.dequeue.c
@@ -0,0 +1,56 @@
+// Implementation using an array in an queue data structure :
+
+// in an queue we use implementation using an array to store the elements of the queue. 
+// we use to indexes which is used for operation : insertion and deletion opearations .
+// 1) front 
+// 2) rear 
+
+// the first element of the queue is known as the front 
+// the last element of the queue is known as the rear 
+
+// there are certain concepts when we use an array to implement data structure :
+
+
+// 1) when the array is an empty :
+
+front = -1
+rear = -1
+
+// when an single element in an array :
+
+front = 0
+rear = 0
+
+// when an array is full :
+
+front = 0 
+rear = size/n-1
+
+// so basically the concept of overflow comes when the queue is full 
+
+// so what is  overflow and what is underflow :
+// 1) overflow :
+         when an array is full and we have to insert an element and there is no space in an queue then the overflow concept occurs . 
+
+
+// 2) so what is underflow :
+// 2) underflow :
+            when an array empty and we have to delete the element and there is no element in an queue then occurs the concept of underflow .
+
+
+// dequeue algorithm :
+dequeue ( queue[], front,rear) // we have taken one queue , first element and last element to perform dequeue operation .
+{
+    if(front == rear == -1)     // this shows the array is an empty underflow condition occurs.
+    printf("underflow");
+    return ;
+
+    if (front == rear) // this shows the array and front = same element and deletion in an same element . 
+    front = rear = -1;
+    else 
+    front = (front+1)modn;
+}
+
+
+// run time complexity of the algorithms is known as O(1) :
+// why because only the several steps has been taken to delete the elemement in an queue data structure :
