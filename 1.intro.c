f85d319d8a5f3b5ea69f521abc0a6f8edd5646ab
balaji <balu.www.55@gmail.com>
2026-09-06 11:09:52 +0530

    commit

---
+++ 1.intro.c
@@ -0,0 +1,20 @@
+// Queue :
+// queue is a linear data structure which comes in priority class .
+
+// queue is known as FIFO (first in first out) list which takes input first and deletes tht first 
+// eg: for queue is tirupati darshan queue who enters first they comes out at first 
+// this is the basic defination of queue 
+
+// there are two opereation on  queue :
+// 1) enqueue : 
+// 2) dequeue :
+
+// 1) Enqueue : 
+// the operation of an insertion in an queue is known as enqueue operation .
+
+// 2) dequeue : 
+// the operation of deletion in an queue is known as dequeue operation .
+
+// queue can be implemented using array and linked list :
+
+// these are the introduction of an queue and its opertions and its implementation concepts . 
