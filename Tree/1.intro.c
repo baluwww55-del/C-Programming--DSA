72f66936c75b2b2a5da96b1e78d3486e0e01eb2b
Balu <balu.www.55@gmail.com>
2026-09-17 06:06:03 +0530

    Add files via upload

---
+++ Tree/1.intro.c
@@ -0,0 +1,36 @@
+// Trees :
+// introduction to tree data structures :
+// tree is a non linear hirearichal data structures that consists of nodes connected by edges 
+
+// there are tree termonlogies :
+// 1) root 
+// 2) child
+// 3) siblings
+// 4) subtree 
+// 5) internal nodes 
+// 6) external nodes 
+
+             5     ---> root node 
+            / \
+           /   \
+          7     4  ----> child nodes and siblings of each other 
+         / \    |
+        1   8   2  -----> leaf nodes /external nodes 
+// 7 below are known as subtree 
+// above leaf nodes are known as internal nodes 
+// depth of the tree is = 3 and height as well 
+
+// these are the tree terminologies and the tree explanation of their concept .
+
+// Handshaking lemma :
+// total no of nodes = total no leaf nodes(l) + total no of internal nodes (i)
+
+// height of a tree :
+
+// we have several formual to find the height of a tree in this we use first defination 
+// height of a tree is = maximum level no 
+// empty tree = -1
+// tree is the no of edges path from the root . 
+// find the shortest path 
+
+// this is the basic introduction of the tree data structure which is non linear data structure 
