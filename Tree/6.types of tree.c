00286950ee8adb4d289f93eba8da4248d9a85ec8
Balu <balu.www.55@gmail.com>
2026-09-22 17:43:05 +0530

    Add files via upload

---
+++ Tree/6.types of tree.c
@@ -0,0 +1,43 @@
+// Types of binary trees:
+
+// majorly there are three types of binary tree :
+// 1) complete binary tree 
+// 2) full binary tree 
+// 3) 3array tree 
+
+// complete binary tree :
+// A binary tree in which every level except the possibly last is completely filled and all nodes are as far as left as possible. 
+
+// this is known as complete binary tree :
+
+// strict binary tree :
+// it is a binary tree allows only all leaf nodes are completely filled last left possible not valid for this tree . 
+
+// NOTE: every strict complete binary tree is a complete binary tree but every binary tree is not a strict binary tree .
+// *) array is used to represent the complete binary tree . 
+
+// left child of node at index 2i. 
+// right child of a node index 2i+1. 
+
+// full binary tree :(sometime proper binary tree or 2-tree) is a tree  in which every node other than the leaf has two children 
+// for binary tree l = i2+1
+// for full binary tree l = i+1. 
+
+// NOTE: no of nodes in a full binary tree is always an odd value . 
+
+// 3-array tree : each node has 0 or 3 children. 
+// l = I2+1
+// this is the formula to find the leaf node of a 3 array tree . 
+
+
+// and another two tree which are :
+// 1) right skewed binarytree :
+// 2) left skewed binarytree :
+
+// 1) right skewed binary tree :
+//   in right skewed binary tree leaf always one . 
+// and  all the internal and leaf nodes always to the right side of the tree . 
+
+// 2) left skewed binary tree :
+// in left skewed binary tree leaf always one . 
+// and all the internal nodes and the leaf nodes always to the left side of the tree . 
